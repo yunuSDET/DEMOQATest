ad616d3737572da6044c5255dd2ed0a3574c734b
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-09-27 01:35:51 +0300

    LoginStepDefinitions class created and snippets added

---
+++ src/test/java/com/demoqa/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,32 @@
+package com.demoqa.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+    @When("user enters userName")
+    public void user_enters_user_name() {
+
+    }
+    @When("user enters password")
+    public void user_enters_password() {
+
+    }
+    @When("user click login button")
+    public void user_click_login_button() {
+
+    }
+    @Then("user should land on the profile page")
+    public void user_should_land_on_the_profile_page() {
+
+    }
+
+
+}
