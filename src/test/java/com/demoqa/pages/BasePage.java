f5faef30acb9ba404e4fee9b0cf235be3a123cd7
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-09-27 01:08:50 +0300

    BasePage created

---
+++ src/test/java/com/demoqa/pages/BasePage.java
@@ -0,0 +1,37 @@
+package com.demoqa.pages;
+
+import com.demoqa.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//span[.='Login']")
+    public WebElement linkLogin;
+
+    @FindBy(xpath = "//span[.='Book Store']")
+    public WebElement linkBookStore;
+
+
+    @FindBy(xpath = "//span[.='Profile']")
+    public WebElement linkProfile;
+
+
+    @FindBy(id = "submit")
+    public WebElement linkLogout;
+
+
+    @FindBy(id = "userName-value")
+    public WebElement userNameValue;
+
+
+
+
+    
+
+}
