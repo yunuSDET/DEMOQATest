5ca9a9168df74964d630582cace8bf63a8aeca3d
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-11-16 03:26:46 +0300

    Random browser functionality is added

--- src/test/java/com/demoqa/utility/Driver.java
+++ src/test/java/com/demoqa/utility/Driver.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -21,6 +25,12 @@ public class Driver {
         if (driver == null) {
 
             String browserType = ConfigurationReader.getProperties("browser");
+
+            if(ConfigurationReader.getProperties("randomBrowser").equalsIgnoreCase("true")){
+                List<String> browserTypes = new ArrayList<>();
+                browserTypes.addAll(Arrays.asList("chrome","firefox","edge"));
+                browserType=browserTypes.get(new Random().nextInt(browserTypes.size()));
+            }
 
             if (browserType.equals("chrome")) {
 
